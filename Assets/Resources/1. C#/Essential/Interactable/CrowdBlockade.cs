f24720402ca38cca72e9d95f56cfcff9ecfe96a1
Maximillian-520 <maximilliankenas520@gmail.com>
2026-03-22 12:12:57 +0700

    Added crowd blockade

---
+++ Assets/Resources/1. C#/Essential/Interactable/CrowdBlockade.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using System.Collections;
+
+public class CrowdBlockade : MonoBehaviour, IInteractable
+{
+    [Header("QUEST TWEAK")]
+    [SerializeField] private BoxCollider2D barrier;
+    [SerializeField] private InteractableObject interactableComponent;
+    [SerializeField] private Door cafeteriaDoor;
+
+    [HideInInspector] public bool hasIntroduced;
+
+    void Start(){
+        interactableComponent = GetComponent<InteractableObject>();
+        if(interactableComponent == null) interactableComponent = gameObject.AddComponent<InteractableObject>();
+            
+        interactableComponent.enabled = false;
+        cafeteriaDoor.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D coll){
+        if(coll.CompareTag("Player") && !DialogueManager.Instance.IsTyping){
+            if(!hasIntroduced) StartCoroutine(IntroduceProblem());
+            else StartCoroutine(RemindProblem());
+        }
+    }
+
+    public void Interact() => StartCoroutine(RemindProblem());
+
+    IEnumerator IntroduceProblem(){
+        yield return new WaitForSeconds(0.1f);
+
+        bool uiReady = false;
+        DialogueManager.Instance.ShowDialogueUI(() => uiReady = true);
+        yield return new WaitUntil(() => uiReady);
+        
+        PlayerCam.Instance.FocusOnTarget(this.transform);
+        DialogueManager.Instance.SetDialogue(
+            DLib.CROWD,
+            "(Crowd sounds crowding)"
+        );
+        yield return new WaitWhile(() => DialogueManager.Instance.IsTypingActive());
+        
+        PlayerCam.Instance.ReturnToPlayer();
+        DialogueManager.Instance.SetDialogue(
+            DLib.PLAYER,
+            "Excuse me, can I please get through?"
+        );
+        yield return new WaitWhile(() => DialogueManager.Instance.IsTypingActive());
+
+        PlayerCam.Instance.FocusOnTarget(this.transform);
+        DialogueManager.Instance.SetDialogue(
+            DLib.CROWD,
+            "(Crowd sounds crowding)"
+        );
+        yield return new WaitWhile(() => DialogueManager.Instance.IsTypingActive());
+        
+        PlayerCam.Instance.ReturnToPlayer();
+        DialogueManager.Instance.SetDialogue(
+            DLib.PLAYER,
+            "I can't get through, maybe I'll go around."
+        );
+        yield return new WaitWhile(() => DialogueManager.Instance.IsTypingActive());
+
+        PlayerCam.Instance.FocusOnTarget(this.transform);
+        DialogueManager.Instance.SetDialogue(
+            DLib.CROWD,
+            "(Crowd sounds crowding)"
+        );
+        yield return new WaitWhile(() => DialogueManager.Instance.IsTypingActive());
+
+        PlayerCam.Instance.ReturnToPlayer();
+        DialogueManager.Instance.HideDialogueUI();
+        ObjectiveUI.Instance.SetObjective("Find a way to pass the crowd");
+        hasIntroduced = true;
+        cafeteriaDoor.enabled = true;
+    }
+
+    IEnumerator RemindProblem(){
+        yield return new WaitForSeconds(0.1f);
+
+        bool uiReady = false;
+        DialogueManager.Instance.ShowDialogueUI(() => uiReady = true);
+        yield return new WaitUntil(() => uiReady);
+        
+        PlayerCam.Instance.FocusOnTarget(this.transform);
+        DialogueManager.Instance.SetDialogue(
+            DLib.CROWD,
+            "(Crowd sounds crowding)"
+        );
+        yield return new WaitWhile(() => DialogueManager.Instance.IsTypingActive());
+
+        PlayerCam.Instance.ReturnToPlayer();
+        DialogueManager.Instance.HideDialogueUI();
+    }
+}
